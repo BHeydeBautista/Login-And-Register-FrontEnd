f460f0bab4ca125593590d05f6c4d963e35cfe5a
Bautista Heyde <bautistaheyde@hotmail.com>
2026-01-14 14:11:35 -0300

    info: Controles para el registro de User / Email en la db

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -8,6 +8,39 @@ export async function POST(request: any) {
 
   const { email, username, password } = data;
 
+  const userFound = await prisma.user.findUnique({
+    where: {
+      email: data.email,
+    },
+  });
+  const usernameFound = await prisma.user.findUnique({
+    where: {
+      email: data.username,
+    },
+  });
+
+  if (userFound) {
+    return NextResponse.json(
+      {
+        message: "Email already exists",
+      },
+      {
+        status: 400,
+      }
+    );
+  }
+
+  if (usernameFound) {
+    return NextResponse.json(
+      {
+        message: "User already exists",
+      },
+      {
+        status: 400,
+      }
+    );
+  }
+
   const newUser = await prisma.user.create({
     data: {
       email,
